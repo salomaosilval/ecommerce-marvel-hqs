345fd32415f1484966760e18cee2bef8748b9c92
Salomão Silva <salomaosilval@hotmail.com>
2023-03-11 18:07:59 -0400

    :sparkles: feat: change % of html to use rem

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -6,6 +6,11 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  html {
+    font-size: 62.5%;
+  }
+
   body {
     font-family: 'Roboto', sans-serif;
   }
