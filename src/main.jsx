d1472c992910ae65a5990fe9b54cbde0bd8e69da
Salomão Silva <salomaosilval@hotmail.com>
2023-03-11 17:46:39 -0400

    :sparkles: feat: add Global Styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { GlobalStyle } from "./styles/globalStyles";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
