993457620c02b995f6b46f4eeba578647cc70be9
Salomão Silva <salomaosilval@hotmail.com>
2023-03-13 11:35:39 -0400

    :sparkles: feat: change scrollbar color

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -14,4 +14,16 @@ export const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Roboto', sans-serif;
   }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #dddddd;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #ed1d24;
+  }
 `;
