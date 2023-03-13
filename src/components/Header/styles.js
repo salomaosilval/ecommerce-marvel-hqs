10150ea4b702b9bf2f34053244930b77a7fdfdf5
Salomão Silva <salomaosilval@hotmail.com>
2023-03-13 11:41:56 -0400

    :bug: fix: bug where side scroll appears

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export const HeaderContainer = styled.nav`
-  position: fixed;
-  width: 100vw;
   height: 8rem;
   background-color: #000;
   color: #fff;
@@ -11,6 +9,7 @@ export const HeaderContainer = styled.nav`
   justify-content: space-around;
   align-items: center;
   border-bottom: 2px solid #ed1d24;
+  margin-bottom: 3rem;
 
   a img {
     width: 15rem;
@@ -21,4 +20,9 @@ export const CartIcon = styled(AiOutlineShoppingCart)`
   font-size: 2.5rem;
   color: #fff;
   cursor: pointer;
+  transition: color 0.6s ease;
+
+  &:hover {
+    color: #ed1d24;
+  }
 `;
