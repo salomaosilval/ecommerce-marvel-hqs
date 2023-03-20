3314fe2f6461e5959a1ee8a73686e191209aff3a
Salomão Silva <salomaosilval@hotmail.com>
2023-03-20 11:26:47 -0400

    :sparkles: feat: add Link to HQ Detail Page

--- src/components/ComicCard/ComicCard.jsx
+++ src/components/ComicCard/ComicCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import { FormatCurrency } from "../../utils/formatCurrency";
 import { HQSContainer, HQSList } from "./styles";
 
 export const ComicCard = (props) => {
@@ -7,14 +9,22 @@ export const ComicCard = (props) => {
     <HQSContainer>
       <HQSList>
         {comicList.map((comic) => (
-          <div key={comic.id}>
-            <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt={comic.title} />
-            <span>{comic.title}</span>
-            <footer>
-              <span>R$4,99</span>
-              <button>Comprar</button>
-            </footer>
-          </div>
+          <Link to={`/hq/${comic.id}`} key={comic.id}>
+            <div>
+              <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt={comic.title} />
+              <span>{comic.title}</span>
+              <footer>
+                <span>{comic.prices[0].price === 0 ? "Grátis" : FormatCurrency(comic.prices[0].price)}</span>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  Comprar
+                </button>
+              </footer>
+            </div>
+          </Link>
         ))}
       </HQSList>
     </HQSContainer>
