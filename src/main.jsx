d191cd13ec7f5675cf18b2e18e68fb8f917aa4cc
Salomão Silva <salomaosilval@hotmail.com>
2023-03-20 19:58:23 -0400

    :sparkles: feat: add Context on main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import { GlobalStyle } from "./styles/globalStyles";
 
+import { CartProvider } from "./contexts/CartContext";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <CartProvider>
+      <GlobalStyle />
+      <App />
+    </CartProvider>
   </React.StrictMode>
 );
