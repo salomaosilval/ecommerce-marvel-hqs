850ea3ae82bc757c6bea0b7d52d065827febca93
Salomão Silva <salomaosilval@hotmail.com>
2023-03-20 20:01:39 -0400

    :sparkles: feat: add all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home/Home";
+import { HQDetails } from "./pages/HQDetails/HQDetails";
+import { Cart } from "./pages/Cart/Cart";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hq/:id" element={<HQDetails />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
